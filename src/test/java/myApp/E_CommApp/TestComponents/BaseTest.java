4613ccfbff5481f9f01ad8e106ce2b0d917be31f
Abdulla Suhail <abdulla.suhail8@gmail.com>
2022-10-23 11:29:22 +0530

    dasda

--- src/test/java/myApp/E_CommApp/TestComponents/BaseTest.java
+++ src/test/java/myApp/E_CommApp/TestComponents/BaseTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
